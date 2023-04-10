add95519dfe38a9396013f252f486b6aeb8bfe7f
Kinashi <76447411+Kinashii@users.noreply.github.com>
2023-04-10 06:57:17 -0400

    Create kinUGSToggle.cs

---
+++ kinUGSToggle.cs
@@ -0,0 +1,51 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class kinUGSToggle : UdonSharpBehaviour
+{
+    // Global synced toggle made by: Kinashi ღ#2321
+
+    public GameObject objectToToggle;
+    [UdonSynced] public bool isOn;
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.LocalPlayer.isMaster)
+        {
+            if (isOn)
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "toggleOn");
+            }
+            else
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "toggleOff");
+            }
+        }
+    }
+
+    public override void Interact()
+    {
+        if (isOn)
+        {
+            isOn = false;
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "toggleOff");
+        }
+        else
+        {
+            isOn = true;
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "toggleOn");
+        }
+    }
+
+    public void toggleOn()
+    {
+        objectToToggle.SetActive(true);
+    }
+
+    public void toggleOff()
+    {
+        objectToToggle.SetActive(false);
+    }
+}
